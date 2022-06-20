2265fc4eec6d6701575b944d60dcd0e85e4f09a3
Austin <austin@austinfay.com>
2022-06-20 12:30:13 -0600

    refac: converted icon to use shadow root

--- ui/components/icon/icon.jsx
+++ ui/components/icon/icon.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import root from 'https://npm.tfl.dev/react-shadow@19?deps=react@18&dev'
 import _ from 'https://npm.tfl.dev/lodash?no-check'
 
 import classKebab from 'https://tfl.dev/@truffle/utils@0.0.1/legacy/class-kebab.js'
 
 import * as legacyIcons from '../../legacy/icons.js'
-
-import styles from './icon.css' assert { type: 'css' }
-document.adoptedStyleSheets = [...document.adoptedStyleSheets, styles]
 
 export default function Icon (props) {
   const {
@@ -42,45 +40,51 @@ export default function Icon (props) {
   }
 
   return (
-    <Tag
-      className={'c-icon ' + classKebab({
-        isAlignedTop,
-        isAlignedLeft,
-        isAlignedRight,
-        isAlignedBottom,
-        isCentered,
-        isTouchTarget,
-        isClickable,
-        isCircled,
-        hasRippleWhite: hasRipple && color !== 'var(--truffle-color-text)',
-        hasRippleHeader: hasRipple && color === 'var(--truffle-color-text)'
-      })}
-      tabIndex={hasRipple ? 0 : undefined}
-      onClick={onclick}
-      onMouseDown={onmousedown}
-      onTouchStart={ontouchstart}
-      style={{
-        minWidth: isTouchTarget ? touchWidth : '0', // 100% makes having a wrapper div necessary
-        minHeight: isTouchTarget ? touchHeight : '100%', // nec to center
-        width: size,
-        height: `${parseInt(size) * heightRatio}${units}`
-      }}
-    >
-      <svg
-        namespace="http://www.w3.org/2000/svg"
-        viewBox={`0 0 ${viewBox} ${viewBox * heightRatio}`}
+    <root.div>
+      <link
+        rel="stylesheet"
+        href={new URL('icon.css', import.meta.url).toString()}
+      />
+      <Tag
+        className={'c-icon ' + classKebab({
+          isAlignedTop,
+          isAlignedLeft,
+          isAlignedRight,
+          isAlignedBottom,
+          isCentered,
+          isTouchTarget,
+          isClickable,
+          isCircled,
+          hasRippleWhite: hasRipple && color !== 'var(--truffle-color-text)',
+          hasRippleHeader: hasRipple && color === 'var(--truffle-color-text)'
+        })}
+        tabIndex={hasRipple ? 0 : undefined}
+        onClick={onclick}
+        onMouseDown={onmousedown}
+        onTouchStart={ontouchstart}
         style={{
+          minWidth: isTouchTarget ? touchWidth : '0', // 100% makes having a wrapper div necessary
+          minHeight: isTouchTarget ? touchHeight : '100%', // nec to center
           width: size,
           height: `${parseInt(size) * heightRatio}${units}`
         }}
       >
-        <path
+        <svg
           namespace="http://www.w3.org/2000/svg"
-          d={legacyIcons[`${_.camelCase(icon)}IconPath`] ?? icon}
-          fill={color}
-          fillRule="evenodd"
-        />
-      </svg>
-    </Tag>
+          viewBox={`0 0 ${viewBox} ${viewBox * heightRatio}`}
+          style={{
+            width: size,
+            height: `${parseInt(size) * heightRatio}${units}`
+          }}
+        >
+          <path
+            namespace="http://www.w3.org/2000/svg"
+            d={legacyIcons[`${_.camelCase(icon)}IconPath`] ?? icon}
+            fill={color}
+            fillRule="evenodd"
+          />
+        </svg>
+      </Tag>
+    </root.div>
   )
 }
