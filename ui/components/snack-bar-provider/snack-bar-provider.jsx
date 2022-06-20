39550459cd457755cb14c843e95f85502d1935e4
Austin <austin@austinfay.com>
2022-06-20 12:36:46 -0600

    refac: converted snack-bar-provider to shadow root

--- ui/components/snack-bar-provider/snack-bar-provider.jsx
+++ ui/components/snack-bar-provider/snack-bar-provider.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useMemo, createContext, useContext } from 'react'
+import root from 'https://npm.tfl.dev/react-shadow@19?deps=react@18&dev'
 
 import { createSubject, op } from 'https://tfl.dev/@truffle/utils@0.0.1/obs/subject.js'
 import useObservables from 'https://tfl.dev/@truffle/utils@0.0.1/obs/use-observables.js'
 
-import styles from './snack-bar-provider.css' assert { type: 'css' }
-document.adoptedStyleSheets = [...document.adoptedStyleSheets, styles]
-
 class SnackBarSerivce {
-
   _queueSubject
 
   constructor () {
@@ -29,7 +26,6 @@ export const snackBarContext = createContext(new SnackBarSerivce())
 const DEFAULT_VISIBILITY_DURATION_MS = 5000
 
 export default function SnackBarProvider ({ children, visibilityDuration = DEFAULT_VISIBILITY_DURATION_MS }) {
-
   const snackBarService = useContext(snackBarContext)
   const snackBarQueueSubject = snackBarService.queueSubject
 
@@ -45,14 +41,19 @@ export default function SnackBarProvider ({ children, visibilityDuration = DEFAU
   useEffect(() => {
     // after a specified duration, pop off the snackbar at the front of the queue;
     // this will trigger another execution of this function by useEffect
-    if (snackBarQueue.length > 0)
-      setTimeout(() => snackBarQueueSubject.next(snackBarQueue.slice(1)), visibilityDuration)
+    if (snackBarQueue.length > 0) {
+      setTimeout(() => snackBarQueueSubject.next(snackBarQueueSubject.getValue().slice(1)), visibilityDuration)
+    }
   }, [snackBarQueue])
 
   return (
-    <>
+    <root.div>
+      <link
+        rel="stylesheet"
+        href={new URL('snack-bar-provider.css', import.meta.url).toString()}
+      />
       <div className='c-snack-bar-container'>{ shouldRenderSnackBar && <$currentSnackBar /> }</div>
       { children }
-    </>
+    </root.div>
   )
 }
