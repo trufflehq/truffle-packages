6334cb3b76f3c8bcf6d59fd25e774f57fa4c6def
Austin <austin@austinfay.com>
2022-06-20 14:22:35 -0600

    refac: snack bar shadow conversion

--- ui/components/snack-bar/snack-bar.jsx
+++ ui/components/snack-bar/snack-bar.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
-import styles from './snack-bar.css' assert { type: 'css' }
-document.adoptedStyleSheets = [...document.adoptedStyleSheets, styles]
+import root from 'https://npm.tfl.dev/react-shadow@19?deps=react@18&dev'
 
 const DEFAULT_VISIBILITY_DURATION_MS = 4000
 const DEFAULT_FADE_DURATION = 500
@@ -20,24 +18,30 @@ export default function SnackBar ({
 }) {
   const fadeOutDelay = visibilityDuration + fadeDuration
   return (
-    <div
-      className={`c-snack-bar-el ${style} ${className}`}
-      style={{
-        animation: `slideIn linear ${fadeDuration}ms, stayVisible linear ${visibilityDuration}ms ${fadeDuration}ms, slideOut linear ${fadeDuration}ms ${fadeOutDelay}ms`
-      }}
-    >
-      <div className='message'
+    <root.div>
+      <link
+        rel="stylesheet"
+        href={new URL('snack-bar.css', import.meta.url).toString()}
+      />
+      <div
+        className={`c-snack-bar-el ${style} ${className}`}
         style={{
-          background: messageBgColor,
-          color: messageTextColor
+          animation: `slideIn linear ${fadeDuration}ms, stayVisible linear ${visibilityDuration}ms ${fadeDuration}ms, slideOut linear ${fadeDuration}ms ${fadeOutDelay}ms`
         }}
-      >{ message }</div>
-      <div className='value'
-        style={{
-          background: valueBgColor,
-          color: valueTextColor
-        }}
-      >{ value }</div>
-    </div>
+      >
+        <div className='message'
+          style={{
+            background: messageBgColor,
+            color: messageTextColor
+          }}
+        >{ message }</div>
+        <div className='value'
+          style={{
+            background: valueBgColor,
+            color: valueTextColor
+          }}
+        >{ value }</div>
+      </div>
+    </root.div>
   )
 }
