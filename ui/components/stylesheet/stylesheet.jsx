9db1eb3abd8f812dad047377d4d2ccc53ad505d1
Austin Hallock <austinhallock@gmail.com>
2022-06-26 17:46:54 -0600

    hide host element until stylesheet is loaded in

--- ui/components/stylesheet/stylesheet.jsx
+++ ui/components/stylesheet/stylesheet.jsx
@@ -1,9 +1,22 @@
-import React from 'https://npm.tfl.dev/react'
+import React, { useRef, useLayoutEffect, useState } from 'https://npm.tfl.dev/react'
 
 // w/o the memo, browser css will flash every render if cache is disabled
 const MemoizeStylesheet = React.memo(({ url }) => {
+  // if in web component, hide until stylesheet is loaded in, to prevent fouc
+  const ref = useRef()
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useLayoutEffect(() => {
+    ref.current.onload = () => {
+      setIsLoaded(true)
+    }
+  }, [])
+  
   return (
-    <link rel="stylesheet" href={url.toString()} />
+    <>
+      <style>{`:host { opacity: ${isLoaded ? 'inherit' : '0'} }`}</style>
+      <link ref={ref} rel="stylesheet" href={url.toString()} />
+    </>
   )
 }, (prevProps, nextProps) => prevProps.url.toString() === nextProps.url.toString())
 
