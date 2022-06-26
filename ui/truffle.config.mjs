0a62827f68074c61800b22e3278e50966860d0c3
Austin Hallock <austinhallock@gmail.com>
2022-06-26 15:48:25 -0600

    ui staging config

--- ui/truffle.config.mjs
+++ ui/truffle.config.mjs
@@ -2,6 +2,6 @@ export default {
   name: '@truffle/ui',
   version: '0.0.1',
   // apiUrl: 'https://mycelium.truffle.vip/graphql',
-  // apiUrl: 'https://mycelium.staging.bio/graphql'
-  apiUrl: 'http://localhost:50420/graphql'
+  apiUrl: 'https://mycelium.staging.bio/graphql'
+  // apiUrl: 'http://localhost:50420/graphql'
 }
