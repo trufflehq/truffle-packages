3ebe643f079d509353b8b35eab03fc184565dfb8
Austin Hallock <austinhallock@gmail.com>
2022-06-26 17:47:05 -0600

    note about useRe

--- ui/components/scoped-stylesheet/scoped-stylesheet.jsx
+++ ui/components/scoped-stylesheet/scoped-stylesheet.jsx
@@ -3,6 +3,9 @@ import root from "https://npm.tfl.dev/react-shadow@19";
 
 import Stylesheet from "../stylesheet/stylesheet.jsx"
 
+// NOTE: ref={ref} in an element inside web component won't work outside of the <ScopedStylesheet>
+// we might want to rename ScopedStylesheet to <Encapsulate>
+// and have devs include <Stylesheet>s manually
 export default function ScopedStylesheet ({ children, url, element = 'div' }) {
   const ShadowRoot = root[element]
   return (
