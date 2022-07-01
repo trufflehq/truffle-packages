eb2c282ccd89ce4c5fb7ecef5ee54a5831c9425a
Riley Miller <rmllr000@gmail.com>
2022-07-01 11:24:40 -0600

    refac: update supabase handler to mycelium api url

--- default-supabase/backend/supabase/functions/viewer-polls-example-handler/api/truffle/fetch.ts
+++ default-supabase/backend/supabase/functions/viewer-polls-example-handler/api/truffle/fetch.ts
@@ -1,4 +1,4 @@
-const TRUFFLE_API_URL = "https://c0e4-2601-285-680-370-bc14-1217-8d64-9c71.ngrok.io/graphql";
+const TRUFFLE_API_URL = "https://mycelium.staging.bio/graphql";
 
 export function truffleFetch(
   query: string,
