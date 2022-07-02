0048fa44d2fadbb6f80d15288983cd1c4b5298cf
Riley Miller <rmllr000@gmail.com>
2022-07-02 15:42:36 -0600

    feat: events-demo-backend package

---
+++ events-demo-backend/backend/supabase/functions/viewer-polls-example-handler/api/truffle/fetch.ts
@@ -0,0 +1,18 @@
+// const TRUFFLE_API_URL = "https://c4b4-98-142-186-46.ngrok.io/graphql";
+const TRUFFLE_API_URL = "https://mycelium.staging.bio/graphql";
+
+export function truffleFetch(
+  query: string,
+  variables: Record<string, unknown>,
+) {
+  return fetch(TRUFFLE_API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+    headers: new Headers({
+      "Authorization": `Bearer ${Deno.env.get("TRUFFLE_API_KEY")}`,
+    }),
+  });
+}
