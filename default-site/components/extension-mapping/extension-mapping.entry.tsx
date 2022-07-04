cb8a1b178f53d71dd6e998d68bbd3c5120de4240
Austin Hallock <austinhallock@gmail.com>
2022-07-04 16:20:36 -0600

    npx vite exeriment

--- default-site/components/extension-mapping/extension-mapping.entry.tsx
+++ default-site/components/extension-mapping/extension-mapping.entry.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "https://npm.tfl.dev/react";
 import jumper from "https://tfl.dev/@truffle/utils@0.0.1/jumper/jumper.js";
-import ScopedStylesheet from "https://tfl.dev/@truffle/ui@0.0.1/components/scoped-stylesheet/scoped-stylesheet.js";
+import Stylesheet from "https://tfl.dev/@truffle/ui@0.0.1/components/stylesheet/stylesheet.js";
+import Button from "https://tfl.dev/@truffle/ui@0.0.2/components/button/button.entry.js";
+import toWebComponent from "https://tfl.dev/@truffle/utils@0.0.1/web-component/to-web-component.js";
 
 import Counter from "../counter/counter.tsx";
 
-export default function ExtensionMapping() {
+function ExtensionMapping() {
   useEffect(() => {
     const style = {
       width: "400px",
@@ -24,9 +26,12 @@ export default function ExtensionMapping() {
   }, []);
 
   return (
-    <ScopedStylesheet url={new URL("./extension-mapping.css", import.meta.url)}>
+    <>
+      <Stylesheet url={new URL("./extension-mapping.css", import.meta.url)} />
       This is my extension mapping
       <Counter initialCount={2} />
-    </ScopedStylesheet>
+    </>
   );
 }
+
+export default toWebComponent(ExtensionMapping)
---
+++ vite-test/truffle.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  name: '@truffle-dev-early-access/austin-sandbox',
+  version: '0.1.1',
+  apiUrl: 'https://mycelium.staging.bio/graphql'
+}
