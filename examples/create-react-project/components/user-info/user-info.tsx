0a194aba746e2ae3c82c1d969c7a24731a056ce9
Austin Hallock <austinhallock@gmail.com>
2022-07-07 16:19:08 -0600

    stylesheet bump

--- examples/create-react-project/components/user-info/user-info.tsx
+++ examples/create-react-project/components/user-info/user-info.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "https://npm.tfl.dev/react";
 import PropTypes from "https://npm.tfl.dev/prop-types@15";
 import AuthDialog from "https://tfl.dev/@truffle/ui@0.0.2/components/auth-dialog/auth-dialog.entry.js";
 import Button from "https://tfl.dev/@truffle/ui@0.0.2/components/button/button.entry.js";
-import Stylesheet from "https://tfl.dev/@truffle/ui@0.0.1/components/stylesheet/stylesheet.js";
+import Stylesheet from "https://tfl.dev/@truffle/ui@0.0.2/components/stylesheet/stylesheet.js";
 import { gql, useQuery } from "https://tfl.dev/@truffle/api@0.0.1/client.js";
 
 const USER_GET_ME_QUERY = gql`
