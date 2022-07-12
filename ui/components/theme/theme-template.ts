1764ff8846d918da060139ea46606e5324661ba7
Austin Hallock <austinhallock@gmail.com>
2022-07-12 15:04:31 -0600

    rm opinionated styles from html/body

--- ui/components/theme/theme-template.ts
+++ ui/components/theme/theme-template.ts
@@ -1,24 +1,17 @@
 import { html } from "https://npm.tfl.dev/@microsoft/fast-element@beta";
 
+// only want to reset styles so first (theme/page) component can style however they want
 export default html`
   <link rel="stylesheet" href=${new URL("./variables.css", import.meta.url)} />
   <style>
     html, body, #root {
       width: 100%;
       height: 100%;
-    }
-    :root {
-      background: var(--tfl-color-bg-fill);
-      color: var(--tfl-color-on-bg-fill);
-      font-family: var(--tfl-font-family-body-sans);
+      padding: 0;
+      margin: 0;
     }
     /* avoid fouc */
     :not(:defined) {
       visibility: hidden;
     }    
-  </style>
-
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
-    rel="stylesheet"
-  />`;
+  </style>`;
