0fadd5dcb253742d9922f786ab96ef3f9658fa05
Austin Hallock <austinhallock@gmail.com>
2022-07-13 10:22:12 -0600

    bump create-react-project to create new layout route

--- examples/create-react-project/truffle.config.mjs
+++ examples/create-react-project/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/create-react-project",
-  version: "0.4.0",
+  version: "0.4.1",
   // apiUrl: 'https://mycelium.truffle.vip/graphql',
   apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql'
