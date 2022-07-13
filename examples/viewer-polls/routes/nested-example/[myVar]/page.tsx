85ae92626aec0cc9e03326d19d9af926c341cba4
Riley Miller <rmllr000@gmail.com>
2022-07-12 22:09:54 -0600

    chore: update nested js

--- examples/viewer-polls/routes/nested-example/[myVar]/page.tsx
+++ examples/viewer-polls/routes/nested-example/[myVar]/page.tsx
@@ -1,5 +1,5 @@
 import React from "https://npm.tfl.dev/react";
-import { useParams } from "https://tfl.dev/@truffle/router@^1.0.0/index.js";
+import { useParams } from "https://tfl.dev/@truffle/router@^1.0.0/index.ts";
 import { toDist } from "https://tfl.dev/@truffle/distribute@^1.0.0/format/wc/index.ts";
 
 function MyVarPage() {
