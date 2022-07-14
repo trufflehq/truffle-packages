78a6508108af04b697f683756e2226920135848f
Austin Hallock <austinhallock@gmail.com>
2022-07-14 15:41:19 -0600

    bump mo example

--- examples/mutation-observer/truffle.config.mjs
+++ examples/mutation-observer/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mutation-observer",
-  version: "0.4.1",
+  version: "0.4.2",
   // apiUrl: 'https://mycelium.truffle.vip/graphql',
   apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql'
