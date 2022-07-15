f976f0a48a1b2dc98352d482c873962548ad236e
Austin Hallock <austinhallock@gmail.com>
2022-07-15 11:03:48 -0600

    switch user-info to useStyleSheet and version create-react-project

--- examples/create-react-project/components/user-info/user-info.tsx
+++ examples/create-react-project/components/user-info/user-info.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from "https://npm.tfl.dev/react";
 import AuthDialog from "https://tfl.dev/@truffle/ui@~0.1.0/components/auth-dialog/auth-dialog.tag.ts";
 import Button from "https://tfl.dev/@truffle/ui@~0.1.0/components/button/button.tag.ts";
-import Stylesheet from "https://tfl.dev/@truffle/ui@~0.1.0/components/stylesheet/stylesheet.tag.ts";
 import { gql, useQuery } from "https://tfl.dev/@truffle/api@~0.1.0/client.ts";
+import { useStyleSheet } from "https://tfl.dev/@truffle/distribute@^2.0.0/format/wc/react/index.ts";
+import styleSheet from "./user-info.css.js";
 
 const USER_GET_ME_QUERY = gql`
   query { me { id, name } }
 `;
 
 export default function UserInfo() {
+  useStyleSheet(styleSheet);
   const [meResult] = useQuery({ query: USER_GET_ME_QUERY });
   const [isAuthDialogHidden, setIsAuthDialogHidden] = useState(true);
 
@@ -16,7 +18,6 @@ export default function UserInfo() {
 
   return (
     <>
-      <Stylesheet url={new URL("./user-info.css", import.meta.url)} />
       <h3>User info</h3>
       <div>ID: {id}</div>
       <div>Name: {name}</div>
--- examples/create-react-project/truffle.config.mjs
+++ examples/create-react-project/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/create-react-project",
-  version: "0.4.1",
+  version: "0.5.0",
   // apiUrl: 'https://mycelium.truffle.vip/graphql',
   apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql'
