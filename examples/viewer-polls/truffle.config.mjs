a6723805e9e2691d4313685c14232ef01c1b5a39
Riley Miller <rmllr000@gmail.com>
2022-07-14 18:28:35 -0600

    chore: bump version

--- examples/viewer-polls/truffle.config.mjs
+++ examples/viewer-polls/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: '@truffle/viewer-polls',
-  version: '0.2.2',
+  version: '0.2.3',
   apiUrl: 'https://mycelium.staging.bio/graphql',
 
   // This is used to specify the required permissions that the package has access to
