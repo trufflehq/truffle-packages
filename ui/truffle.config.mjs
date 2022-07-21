4e99c94161f03a2bc6e9d9b661b53f8e3118b9ba
Austin Hallock <austinhallock@gmail.com>
2022-07-21 12:39:49 -0600

    bump truffle ui

--- ui/truffle.config.mjs
+++ ui/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/ui",
-  version: "0.1.0",
+  version: "0.1.1",
   // apiUrl: "https://mycelium.truffle.vip/graphql",
   apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: "http://localhost:50420/graphql",
