b716cfc37126fb44fe2d28e1bf96ea0ae21a5fc7
shanecranor <shanecranor@gmail.com>
2022-07-25 12:04:03 -0600

    bump version number

--- examples/spotify-integration/truffle.config.mjs
+++ examples/spotify-integration/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   // TODO: rename to @truffle/spotify-now-listening for prod
   name: "@truffle/spotify-integration",
-  version: "2.2.8",
+  version: "3.0.0",
   apiUrl: "https://mycelium.staging.bio/graphql",
 };
