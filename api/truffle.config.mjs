feb8d596c1baae1bdd0118ae4e7d18dec39139da
Austin Hallock <austinhallock@gmail.com>
2022-08-03 12:09:47 -0600

    bump api

--- api/truffle.config.mjs
+++ api/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/api",
-  version: "0.1.1",
+  version: "0.1.2",
   // apiUrl: "https://mycelium.truffle.vip/graphql",
   apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql'
