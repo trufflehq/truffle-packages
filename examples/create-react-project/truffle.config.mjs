d2ca897700af48f32a2acc6c6b0b7458fef7502c
Austin Hallock <austinhallock@gmail.com>
2022-08-27 18:18:28 -0600

    spec for embeds in truffle config

--- examples/create-react-project/truffle.config.mjs
+++ examples/create-react-project/truffle.config.mjs
@@ -3,5 +3,24 @@ export default {
   version: "0.5.6",
   // apiUrl: "https://mycelium.truffle.vip/graphql",
   apiUrl: "https://mycelium.staging.bio/graphql",
-  // apiUrl: 'http://localhost:50420/graphql'
+  // apiUrl: 'http://localhost:50420/graphql',
+  embeds: [
+    // TODO: implement as part of deploy step
+    // TODO: somehow make these only visible for dev mode until approved by us?
+    {
+      route: "/hidden-extension-mapping",
+      sourceType: "url",
+      sourceId: "https://www.youtube.com/watch?v=Xe5KJINZGRA",
+      defaultLayoutConfigSteps: [
+        {
+          action: "querySelector",
+          value: "body",
+        },
+        {
+          action: "appendSubject",
+        },
+      ],
+      status: "experimental",
+    },
+  ],
 };
