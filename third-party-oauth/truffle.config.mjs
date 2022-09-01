faa56aa408da7db86bd16483510660f27f5273e3
Riley Miller <rmllr000@gmail.com>
2022-09-01 16:54:04 -0600

    chore: bump

--- third-party-oauth/truffle.config.mjs
+++ third-party-oauth/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/third-party-oauth",
-  version: "0.0.7",
+  version: "0.0.8",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   // apiUrl: 'http://172.16.100.130:50420/graphql',
   requestedPermissions: [],
