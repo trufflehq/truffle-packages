6c1f7a88c1baaeabeb60b16f0571067ba2a8c04e
Riley Miller <rmllr000@gmail.com>
2022-09-07 15:53:06 -0600

    feat: extension login utils

--- utils/embed/helpers.ts
+++ utils/embed/helpers.ts
@@ -1,8 +1,26 @@
+import jumper from "https://tfl.dev/@truffle/utils@0.0.3/jumper/jumper.ts";
+import { _setAccessTokenAndClear } from "https://tfl.dev/@truffle/api@~0.1.7/mod.ts";
 import { CONNECTION_SOURCE_TYPES, PageIdentifier } from "./types.ts";
-import { findFirst } from '../helpers/mod.ts'
+import { findFirst } from "../helpers/mod.ts";
+
+export const TRUFFLE_ACCESS_TOKEN_KEY = "mogul-menu:accessToken";
 
 export function getConnectionSourceType(pageIdentifiers: PageIdentifier[]) {
   const sourceTypes = pageIdentifiers.map(({ sourceType }) => sourceType);
 
   return findFirst(CONNECTION_SOURCE_TYPES, sourceTypes);
+}
+
+export async function getTruffleTvAccessToken() {
+  const accessToken = await jumper?.call("storage.get", {
+    key: TRUFFLE_ACCESS_TOKEN_KEY,
+  });
+
+  return accessToken;
+}
+
+export async function loginFromExtension() {
+  const accessToken = await getTruffleTvAccessToken();
+
+  _setAccessTokenAndClear(accessToken);
 }
