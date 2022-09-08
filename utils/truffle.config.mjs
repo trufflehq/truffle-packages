7fce7c83ca9fda28c611c5915a39ee572c4594bb
Riley Miller <rmllr000@gmail.com>
2022-09-08 14:14:10 -0600

    feat: global jumper messages

--- utils/truffle.config.mjs
+++ utils/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/utils",
-  version: "0.0.11",
+  version: "0.0.12",
   apiUrl: "https://mycelium.truffle.vip/graphql",
 //  apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql'
