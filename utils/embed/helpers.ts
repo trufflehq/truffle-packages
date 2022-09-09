04affb8547dab28b3332c22409b45af7da436862
Riley Miller <rmllr000@gmail.com>
2022-09-08 20:56:22 -0600

    fix: fix find first array method

--- utils/embed/helpers.ts
+++ utils/embed/helpers.ts
@@ -1,4 +1,3 @@
-import { _setAccessTokenAndClear } from './deps.ts'
 import { CONNECTION_SOURCE_TYPES, PageIdentifier } from "./types.ts";
 import { findFirst } from "../helpers/mod.ts";
 
@@ -6,6 +5,5 @@ export const TRUFFLE_ACCESS_TOKEN_KEY = "mogul-menu:accessToken";
 
 export function getConnectionSourceType(pageIdentifiers: PageIdentifier[]) {
   const sourceTypes = pageIdentifiers.map(({ sourceType }) => sourceType);
-
   return findFirst(CONNECTION_SOURCE_TYPES, sourceTypes);
 }
