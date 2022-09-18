67c3790e1cea2a8da1e788d1fe889608d106b96d
Riley Miller <rmllr000@gmail.com>
2022-09-17 18:35:07 -0600

    feat: add staging oauth client

--- third-party-oauth/truffle.config.mjs
+++ third-party-oauth/truffle.config.mjs
@@ -1,7 +1,10 @@
 export default {
   name: "@truffle/third-party-oauth",
   version: "0.0.23",
- apiUrl: "https://mycelium.truffle.vip/graphql",
+  apiUrl: "https://mycelium.truffle.vip/graphql",
+  // staging
+  // version: "0.0.27",
+  // apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql',
   requestedPermissions: [],
   installActionRel: {},
