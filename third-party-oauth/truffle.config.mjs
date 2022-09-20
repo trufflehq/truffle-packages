9999d9b9df74b0579776a471026fca90cd007414
Riley Miller <rmllr000@gmail.com>
2022-09-20 10:59:58 -0600

    refac: pass down text variant

--- third-party-oauth/truffle.config.mjs
+++ third-party-oauth/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/third-party-oauth",
-  version: "0.0.30",
+  version: "0.0.31",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   // staging
   // version: "0.0.30",
