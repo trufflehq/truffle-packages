26634d3529b2d07cb14c04161457116ae030078a
Riley Miller <rmllr000@gmail.com>
2022-09-20 10:53:59 -0600

    docs: add deployment notes to README

--- third-party-oauth/truffle.config.mjs
+++ third-party-oauth/truffle.config.mjs
@@ -3,7 +3,7 @@ export default {
   version: "0.0.30",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   // staging
-  // version: "0.0.29",
+  // version: "0.0.30",
   // apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql',
   requestedPermissions: [],
