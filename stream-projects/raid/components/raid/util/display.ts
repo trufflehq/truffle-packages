6fdee75dde660e929ea2c6f0485ad7a127587571
Austin Fay <austin@austinfay.com>
2022-10-06 16:39:26 -0600

    fix: allow for positioning inside video frame

--- stream-projects/raid/components/raid/util/display.ts
+++ stream-projects/raid/components/raid/util/display.ts
@@ -5,9 +5,9 @@ const RAID_IRFRAME_HIDDEN_STYLES = {
 };
 
 const RAID_IFRAME_SHOWING_STYLES = {
-  width: "100vw",
-  height: "100vh",
-  position: "fixed",
+  width: "100%",
+  height: "100%",
+  position: "absolute",
   display: "block",
   "z-index": 999,
 };
