3f7b309a4775349665469a9838122ade9406bcc4
Austin Fay <austin@austinfay.com>
2022-10-06 16:08:00 -0600

    refac: simplify getTwitchParents

--- stream-projects/raid/shared/util/stream-plat.ts
+++ stream-projects/raid/shared/util/stream-plat.ts
@@ -47,54 +47,45 @@ export function extractTwitchChannelName(url: string) {
 
 export function getTwitchParents() {
   const domains = [
-    "spore.build",
-    "truffle-dev.spore.build",
-    "drlupo.spore.build",
-    "lily.spore.build",
-    "myth.spore.build",
-    "sykkuno.spore.build",
-    "jidion.spore.build",
-    "staging.bio",
-    "dev.staging.bio",
-    "truffle.vip",
+    "twitch.tv",
     "www.youtube.com",
-    "patterrz.spore.build",
-    "razzle.spore.build",
-    "purplecliffe.spore.build",
-    "fuslie.spore.build",
-    "valkyrae.spore.build",
-    "ottomated.spore.build",
-    "pointcrow.spore.build",
-    "shylily.spore.build",
-    // this is a temporary hack, we should dynamically resolve the org
-    "ludwig.social",
-    "localhost",
+    location.hostname,
   ];
   return domains.reduce(
     (acc, domain, idx) =>
       `${acc}parent=${domain}${idx === domains.length - 1 ? "" : "&"}`,
-    ""
+    "",
   );
 }
 
 export function previewSrc(url: string) {
-  if (isYoutubeChannel(url))
-    return `https://www.youtube.com/embed/live_stream?channel=${extractYoutubeChannelId(
-      url
-    )}`;
+  if (isYoutubeChannel(url)) {
+    return `https://www.youtube.com/embed/live_stream?channel=${
+      extractYoutubeChannelId(
+        url,
+      )
+    }`;
+  }
 
-  if (isYoutubeVideo(url))
+  if (isYoutubeVideo(url)) {
     return `https://www.youtube.com/embed/${extractYoutubeId(url)}`;
+  }
 
-  if (isTwitchChannel(url))
-    return `https://player.twitch.tv/?channel=${extractTwitchChannelName(
-      url
-    )}&${getTwitchParents()}`;
+  if (isTwitchChannel(url)) {
+    return `https://player.twitch.tv/?channel=${
+      extractTwitchChannelName(
+        url,
+      )
+    }&${getTwitchParents()}`;
+  }
 
-  if (isTwitchVideo(url))
-    return `https://player.twitch.tv/?video=${extractTwitchVideoId(
-      url
-    )}&${getTwitchParents()}`;
+  if (isTwitchVideo(url)) {
+    return `https://player.twitch.tv/?video=${
+      extractTwitchVideoId(
+        url,
+      )
+    }&${getTwitchParents()}`;
+  }
 
   return null;
 }
--- stream-projects/raid/truffle.config.mjs
+++ stream-projects/raid/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/raid",
-  version: "0.0.3",
+  version: "0.0.4",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   requestedPermissions: [],
   installActionRel: {},
