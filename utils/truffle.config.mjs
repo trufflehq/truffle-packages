8452f2d1e03c74feb468baa3a46a98f85680c1dc
Riley Miller <rmllr000@gmail.com>
2022-10-10 14:06:35 -0600

    refac: revert to minor updates

--- utils/truffle.config.mjs
+++ utils/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/utils",
-  version: "0.0.20",
+  version: "0.0.21",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   //  apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql'
