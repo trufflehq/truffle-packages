08aebb407bdbf5ab4a017bb7a63a0fe46ff01065
Carter <me@fyko.net>
2022-10-10 18:48:21 -0600

    chore: delete random doc

--- examples/chants/components/chants/chant.css.js
+++ examples/chants/components/chants/chant.css.js
@@ -1,6 +1,5 @@
 import css from "https://tfl.dev/@truffle/utils@~0.0.3/css/css.ts";
 
-// sass example (make sure file is named ___.scss.js or ___.sass.js)
 export default css`
 @keyframes pop {
   50%  {transform: scale(2);}
