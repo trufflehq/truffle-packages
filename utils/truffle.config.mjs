555e28d345e2668e82d2b150e70fe5369d062878
Riley Miller <rmllr000@gmail.com>
2022-10-17 12:53:18 -0600

    feat: add useExtensionInfo$ hook

--- utils/truffle.config.mjs
+++ utils/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/utils",
-  version: "0.0.21",
+  version: "0.0.22",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Browser & Node utilities",
   //  apiUrl: "https://mycelium.staging.bio/graphql",
