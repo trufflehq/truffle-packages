09aece9096608b8cdb7ed88fc6f6cdb1aa305762
Carter <me@fyko.net>
2022-10-19 16:22:35 -0600

    chore: rebuild map & create chants description

--- examples/chants/truffle.config.mjs
+++ examples/chants/truffle.config.mjs
@@ -1,6 +1,7 @@
 export default {
   name: "@truffle/chants",
   version: "0.0.3",
+  description: "Adding an effectful but lightweight extension of emoji spam in live chat.",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   requestedPermissions: [],
   installActionRel: {},
