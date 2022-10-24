ea919703f4923c134b08604ea1463fbe29ca711c
Austin Hallock <austinhallock@gmail.com>
2022-10-24 10:50:42 -0700

    bumps

--- examples/chants/truffle.config.mjs
+++ examples/chants/truffle.config.mjs
@@ -1,8 +1,9 @@
 export default {
   name: "@truffle/chants",
-  version: "0.0.3",
-  description: "Adding an effectful but lightweight extension of emoji spam in live chat.",
+  version: "0.0.4",
+  description:
+    "Adding an effectful but lightweight extension of emoji spam in live chat.",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   requestedPermissions: [],
   installActionRel: {},
-}
+};
--- third-party-oauth/truffle.config.mjs
+++ third-party-oauth/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/third-party-oauth",
-  version: "0.0.38",
+  version: "0.0.39",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Login and other auth utilities",
   // staging
