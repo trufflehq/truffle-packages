9f30ba99fe708e9c754db8561d4944cd38d679ff
Austin Hallock <austinhallock@gmail.com>
2022-11-22 12:47:12 -0800

    rm subscription code from react example

--- examples/create-react-project/components/user-info/user-info.tsx
+++ examples/create-react-project/components/user-info/user-info.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from "https://npm.tfl.dev/react";
 import AuthDialog from "https://tfl.dev/@truffle/ui@~0.1.0/components/auth-dialog/auth-dialog.tag.ts";
 import Button from "https://tfl.dev/@truffle/ui@~0.1.0/components/button/button.tag.ts";
-import {
-  gql,
-  useQuery,
-  useSubscription,
-} from "https://tfl.dev/@truffle/api@~0.1.15/client.ts";
+import { gql, useQuery } from "https://tfl.dev/@truffle/api@~0.1.15/client.ts";
 import { useStyleSheet } from "https://tfl.dev/@truffle/distribute@^2.0.0/format/wc/react/index.ts"; // DO NOT BUMP;
 
 import styleSheet from "./user-info.css.js";
@@ -18,40 +14,6 @@ export default function UserInfo() {
   useStyleSheet(styleSheet);
   const [meResult] = useQuery({ query: USER_GET_ME_QUERY });
   const [isAuthDialogHidden, setIsAuthDialogHidden] = useState(true);
-  // const [subResult] = useSubscription({
-  //   query: gql`subscription {
-  //     alertConnection(input: { type: "activity" }, first: 2) {
-  //       nodes {
-  //         id
-  //         message
-  //         activity {
-  //           __typename
-  //           ... on Poll {
-  //             id
-  //             question
-  //             options {
-  //                 text
-  //                 index
-  //                 count
-  //                 unique
-  //             }
-  //             myVote {
-  //               optionIndex
-  //               count
-  //             }
-  //           }
-  //           ... on Alert {
-  //             id
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }`,
-  // });
-  // console.log(
-  //   "subscription example",
-  //   JSON.stringify(subResult?.data, null, 2),
-  // );
 
   const { name, id } = meResult.data?.me || {};
 
