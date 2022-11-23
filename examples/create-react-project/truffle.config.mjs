9dc6a35e4199cb38d43f44d069a6198da61e118f
Austin Hallock <austinhallock@gmail.com>
2022-11-22 22:08:26 -0800

    bump create-react-project

--- examples/create-react-project/truffle.config.mjs
+++ examples/create-react-project/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/create-react-project",
-  version: "0.5.7",
+  version: "0.5.8",
   // apiUrl: "https://mycelium.truffle.vip/graphql",
   apiUrl: "https://mycelium.staging.bio/graphql",
   description: "Truffle project React template",
