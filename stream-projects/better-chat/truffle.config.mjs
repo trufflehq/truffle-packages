c26a991bd1e6653132816b73f9223abe8081d880
Austin Hallock <austinhallock@gmail.com>
2022-11-23 22:40:42 -0800

    bump better-chat

--- stream-projects/better-chat/truffle.config.mjs
+++ stream-projects/better-chat/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/better-chat",
-  version: "0.0.8",
+  version: "0.0.10",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   requestedPermissions: [],
   installActionRel: {},
