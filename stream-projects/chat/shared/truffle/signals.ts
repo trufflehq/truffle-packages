50920304b3ed87371c7082dcaddd10a76d562578
Riley Miller <rmllr000@gmail.com>
2022-12-06 15:30:42 -0700

    feat: add support for subscribing via videoId

--- stream-projects/chat/shared/truffle/signals.ts
+++ stream-projects/chat/shared/truffle/signals.ts
@@ -1,27 +1,38 @@
-import { jumper, pageHashParams, useComputed, useObservable, gql } from "../../deps.ts";
-import { getTruffleChatEmoteMapByYoutubeChannelId, getYoutubeChannelId } from "./utils.ts";
-import { OrgUserWithChatInfoConnection } from './types.ts'
+import {
+  gql,
+  jumper,
+  pageHashParams,
+  useComputed,
+  useObservable,
+} from "../../deps.ts";
+import {
+  getTruffleChatEmoteMapByYoutubeChannelId,
+  getYoutubeChannelId,
+  getYoutubeVideoId,
+} from "./utils.ts";
+import { OrgUserWithChatInfoConnection } from "./types.ts";
 import { CONNECTION_FIELDS, ORG_USER_CHAT_INFO_FIELDS } from "./fragments.ts";
-import { useQuerySignal } from '../mod.ts'
+import { useQuerySignal } from "../mod.ts";
 export function useExtensionInfo$() {
   const extensionInfo$ = useObservable(jumper.call("context.getInfo"));
 
   return { extensionInfo$ };
 }
 
-export function useYoutubeChannelId$() {
+export function useYoutubeStreamInfo$() {
   const { extensionInfo$ } = useExtensionInfo$();
   const youtubeChannelId$ = useComputed(() => {
     const extensionInfo = extensionInfo$.get();
 
     // allows passing in a yt channel id via the url hash, can be used during development or for a dynamic channel id via a url
     const paramYoutubeChannelId = pageHashParams.ytChannelId.get();
+    console.log("extensionInfo?.pageInfo", extensionInfo?.pageInfo);
     const youtubeChannelId = getYoutubeChannelId(extensionInfo?.pageInfo);
 
     console.log("EXTENSION INFO", extensionInfo);
     console.log("PAGE INFO", extensionInfo?.pageInfo);
     console.log("CHANNELID", youtubeChannelId);
-    
+
     // return "UCGwu0nbY2wSkW8N-cghnLpA"; // jaiden
     // return "UCrPseYLGpNygVi34QpGNqpA"; // lud
     // return "UCXBE_QQSZueB8082ml5fslg"; // tim
@@ -32,11 +43,17 @@ export function useYoutubeChannelId$() {
     return youtubeChannelId ?? paramYoutubeChannelId;
   });
 
-  return { youtubeChannelId$ };
+  const youtubeVideoId$ = useComputed(() => {
+    const extensionInfo = extensionInfo$.get();
+
+    return getYoutubeVideoId(extensionInfo?.pageInfo);
+  });
+
+  return { youtubeChannelId$, youtubeVideoId$ };
 }
 
 export function useTruffleEmoteMap$() {
-  const { youtubeChannelId$ } = useYoutubeChannelId$();
+  const { youtubeChannelId$ } = useYoutubeStreamInfo$();
 
   const emoteMap$ = useComputed(() => {
     return getTruffleChatEmoteMapByYoutubeChannelId(youtubeChannelId$.get());
@@ -61,7 +78,9 @@ export const ORG_USER_WITH_CHAT_INFO_AND_CONNECTIONS = gql<
 `;
 
 export function useOrgUserWithChatInfoAndConnections$() {
-  const orgUserWithChatInfoAndConnection$ = useQuerySignal(ORG_USER_WITH_CHAT_INFO_AND_CONNECTIONS);
+  const orgUserWithChatInfoAndConnection$ = useQuerySignal(
+    ORG_USER_WITH_CHAT_INFO_AND_CONNECTIONS,
+  );
 
   return { orgUserWithChatInfoAndConnection$ };
 }
