9fa08d08680c4d79e4bc521c2c6c903940af7453
Austin Fay <afay@truffle.vip>
2022-12-12 09:20:16 -0700

    docs: added description

--- route-installer/truffle.config.mjs
+++ route-installer/truffle.config.mjs
@@ -2,6 +2,8 @@ export default {
   name: "@truffle/route-installer",
   version: "0.5.7",
   apiUrl: "https://mycelium.truffle.vip/graphql",
+  description:
+    "Provides an action that allows packages to add their routes into other packages.",
   requestedPermissions: [],
   installActionRel: {},
-}
+};
