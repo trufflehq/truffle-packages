656e2ab1b8437979ff4021cbfc21654e2ea5a714
Austin Fay <afay@truffle.vip>
2022-12-12 15:19:55 -0700

    refac: now using graphql-request

--- functions/truffle.config.mjs
+++ functions/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/functions",
-  version: "0.0.4",
+  version: "0.0.5",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Utilities for creating Truffle Edge Functions.",
   requestedPermissions: [],
--- functions/utils/serve.ts
+++ functions/utils/serve.ts
@@ -1,9 +1,11 @@
 import { ConnInfo, serve } from "https://deno.land/std@0.167.0/http/server.ts";
-import { MyceliumClient } from "./graphql-client.ts";
+import { GraphQLClient } from "https://deno.land/x/graphql_request@v4.1.0/mod.ts";
 import { makeResp } from "./make-resp.ts";
 
+const DEFAULT_MYCELIUM_API_URL = "https://mycelium.truffle.vip/graphql";
+
 interface EdgeFunctionHandlerParams<RuntimeDataType = unknown> {
-  myceliumClient: MyceliumClient;
+  myceliumClient: GraphQLClient;
   request: Request;
   connInfo: ConnInfo;
   runtimeData: RuntimeDataType;
@@ -13,62 +15,75 @@ interface EdgeFunctionHandlerOptions {
   apiUrl?: string;
 }
 
+export interface MyceliumGQLResponse<T = unknown> {
+  data: T;
+  errors: unknown[];
+}
+
 type EdgeFunctionHandler<RuntimeDataType> = (
-  params: EdgeFunctionHandlerParams<RuntimeDataType>,
+  params: EdgeFunctionHandlerParams<RuntimeDataType>
 ) => Response | Promise<Response>;
 
 export function serveTruffleEdgeFunction<RuntimeDataType = unknown>(
   handler: EdgeFunctionHandler<RuntimeDataType>,
-  options?: EdgeFunctionHandlerOptions,
+  options?: EdgeFunctionHandlerOptions
 ) {
-  return serve(async (request, connInfo) => {
-    const accessToken = request.headers.get("x-access-token");
-    const orgId = request.headers.get("x-org-id");
+  return serve(
+    async (request, connInfo) => {
+      const accessToken = request.headers.get("x-access-token");
+      const orgId = request.headers.get("x-org-id");
 
-    // TODO: implement some kind of mechanism to verify that the request is coming from truffle
+      // TODO: implement some kind of mechanism to verify that the request is coming from truffle
 
-    // return an error if they don't provide an access token
-    if (!accessToken) {
-      return makeResp(400, {
-        error: true,
-        message:
-          "Please provide a user access token via the 'x-access-token' header.",
+      // return an error if they don't provide an access token
+      if (!accessToken) {
+        return makeResp(400, {
+          error: true,
+          message:
+            "Please provide a user access token via the 'x-access-token' header.",
+        });
+      }
+
+      // return an error if they don't provide an access token
+      if (!orgId) {
+        return makeResp(400, {
+          error: true,
+          message: "Please provide an org id via the 'x-org-id' header.",
+        });
+      }
+
+      let runtimeData: RuntimeDataType;
+      try {
+        runtimeData = (await request.json())?.runtimeData;
+      } catch {
+        return makeResp(400, {
+          error: true,
+          message: "Please provide a valid JSON body.",
+        });
+      }
+
+      const myceliumClient = new GraphQLClient(
+        options?.apiUrl ?? DEFAULT_MYCELIUM_API_URL,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-org-id": orgId,
+            "x-access-token": accessToken,
+          },
+        }
+      );
+
+      return handler({
+        myceliumClient,
+        request,
+        connInfo,
+        runtimeData,
       });
-    }
-
-    // return an error if they don't provide an access token
-    if (!orgId) {
-      return makeResp(400, {
-        error: true,
-        message: "Please provide an org id via the 'x-org-id' header.",
-      });
-    }
-
-    let runtimeData: RuntimeDataType;
-    try {
-      runtimeData = (await request.json())?.runtimeData;
-    } catch {
-      return makeResp(400, {
-        error: true,
-        message: "Please provide a valid JSON body.",
-      });
-    }
-
-    const myceliumClient = new MyceliumClient(
-      accessToken,
-      orgId,
-      options?.apiUrl,
-    );
-
-    return handler({
-      myceliumClient,
-      request,
-      connInfo,
-      runtimeData,
-    });
-  }, {
-    onListen({ hostname, port }) {
-      console.log(`Listening on http://${hostname}:${port}`);
     },
-  });
+    {
+      onListen({ hostname, port }) {
+        console.log(`Listening on http://${hostname}:${port}`);
+      },
+    }
+  );
 }
