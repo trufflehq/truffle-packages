f6dd059842da2d24560ba67494a99a8d11e28ce9
Riley Miller <rmllr000@gmail.com>
2022-12-15 14:36:23 -0700

    refac: put discoball behind chat messages

--- stream-projects/chat-theme/components/watch-party-theme/watch-party-theme.tsx
+++ stream-projects/chat-theme/components/watch-party-theme/watch-party-theme.tsx
@@ -14,7 +14,7 @@ const setJumperYoutubeStyles = () => {
     left: 0,
     bottom: 0,
     "pointer-events": "none",
-    "z-index": "1",
+    "z-index": "-1",
   };
   jumper.call("layout.applyLayoutConfigSteps", {
     layoutConfigSteps: [
