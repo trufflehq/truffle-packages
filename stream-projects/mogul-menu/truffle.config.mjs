5ef045813d1d6897a0ee11f43bb3d74b37cef753
Riley Miller <rmllr000@gmail.com>
2022-12-23 11:30:03 -0600

    chore: bump

--- stream-projects/mogul-menu/truffle.config.mjs
+++ stream-projects/mogul-menu/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "3.0.87",
+  version: "3.0.88",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Menu for channel points, predictions, and activities",
   requestedPermissions: [],
