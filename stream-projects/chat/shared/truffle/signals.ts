b43358a1af298f9cce29c56d590efbc75ea8a18a
Austin Hallock <austinhallock@gmail.com>
2022-12-26 16:49:18 -0800

    rm myth hardcode

--- stream-projects/chat/shared/truffle/signals.ts
+++ stream-projects/chat/shared/truffle/signals.ts
@@ -38,7 +38,7 @@ export function useYoutubeStreamInfo$() {
     // return "UCrPseYLGpNygVi34QpGNqpA"; // lud
     // return "UCXBE_QQSZueB8082ml5fslg"; // tim
     // return "UCZaVG6KWBuquVXt63G6xopg"; // riley
-    return "UCvQczq3aHiHRBGEx-BKdrcg"; // myth
+    // return "UCvQczq3aHiHRBGEx-BKdrcg"; // myth
     // return "UCG6zBb8GZKo1XZW4eHdg-0Q"; // pcrow
     // return "UCNF0LEQ2abMr0PAX3cfkAMg"; // lupo
     return youtubeChannelId ?? paramYoutubeChannelId;
