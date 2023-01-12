756ebd11daa879d9bdc864f07d018b2bb99bd07a
Austin Fay <austin@austinfay.com>
2023-01-12 15:49:27 -0700

    add: examples

---
+++ npm/sdk/examples/react-basic/src/App.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { embed } from '@trufflehq/sdk'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [isSmall, setIsSmall] = useState(true)
+  const [hasBorder, setHasBorder] = useState(false)
+
+  const setSize = () => {
+    if (isSmall) {
+      embed.setSize("800px", "800px")
+      setIsSmall(false)
+    } else {
+      embed.setSize("600px", "600px")
+      setIsSmall(true)
+    }
+  }
+  
+  const setBorder = () => {
+    if (hasBorder) {
+      embed.setStyles({
+        border: "none"
+      })
+      setHasBorder(false)
+    } else {
+      embed.setStyles({
+        border: "3px solid red"
+      })
+      setHasBorder(true)
+    }
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://truffle.vip" target="_blank">
+          <img src="https://cdn.bio/assets/images/branding/logomark.svg" className="logo truffle" alt="Truffle logo" />
+        </a>
+      </div>
+      <h1>React + Truffle</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <h2>Embed controls</h2>
+      <button onClick={setSize}>Toggle Size</button>
+      <button onClick={setBorder}>Toggle Border</button>
+    </div>
+  )
+}
+
+export default App
---
+++ npm/sdk/examples/vue-basic/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import EmbedController from './components/EmbedController.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <a href="https://truffle.vip" target="_blank">
+      <img src="https://cdn.bio/assets/images/branding/logomark.svg" class="logo truffle" alt="Truffle logo" />
+    </a>
+  </div>
+  <h1>Vue + Truffle</h1>
+  <EmbedController />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.logo.truffle:hover {
+  filter: drop-shadow(0 0 2em #F357A1);
+}
+</style>
