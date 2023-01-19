5c2865dd96080f5df06bf558644e9ffbfd03c1d4
Austin Hallock <austinhallock@gmail.com>
2023-01-19 09:59:47 -0800

    bump

--- stream-projects/twitch-on-youtube/truffle.config.mjs
+++ stream-projects/twitch-on-youtube/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/twitch-on-youtube",
-  version: "0.5.37",
+  version: "0.5.39",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   requestedPermissions: [],
   installActionRel: {},
