22abc123e96fe104110bf49d3ba3a2f5e8216cf2
Austin Fay <afay@truffle.vip>
2023-01-23 15:41:05 -0800

    add: react auth example (#82)
    * add: react auth example
    
    * add: comments

---
+++ npm/sdk/examples/react-auth/src/App.tsx
@@ -0,0 +1,50 @@
+import "./App.css";
+import {
+  user as userClient,
+  getSrcByImageObj,
+  org as orgClient,
+} from "@trufflehq/sdk";
+import { observer } from "@legendapp/state/react";
+import { fromSpecObservable } from "./from-spec-observable";
+import { useEffect, useState } from "react";
+
+// here we're creating observables using the legend state library
+// https://legendapp.com/open-source/state/
+const user = fromSpecObservable(userClient.observable);
+const orgUser = fromSpecObservable(userClient.orgUser.observable);
+const org = fromSpecObservable(orgClient.observable);
+
+function App() {
+
+  // `.observable` is actually a spec compliant observable
+  // https://github.com/tc39/proposal-observable
+  // you can interact with it directly if you want, but we prefer to use legend :p
+  const [orgId, setOrgId] = useState<string | undefined>(undefined);
+  useEffect(() => {
+    const subscription = orgClient.observable.subscribe({
+      next: (org) => {
+        setOrgId(org.id);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {}
+    })
+
+    return () => subscription.unsubscribe()
+  })
+
+  return (
+    <div className="App">
+      {/* when using a legend state observable, you can use the `.get()` method to get the current value */}
+      {/* the cool thing is that it will automatically update on any changes */}
+      <div>Org: {org.name.get()}</div>
+      <div>Org ID: {orgId}</div>
+      <h2>Welcome, {orgUser.name.get()}</h2>
+      <img src={getSrcByImageObj(user.avatarImage.get(), { size: "small" })} />
+    </div>
+  );
+}
+
+// we wrap the app component so that it listens to changes on the legend observables
+export default observer(App);
---
+++ npm/sdk/examples/react-auth/src/from-spec-observable.ts
@@ -0,0 +1,29 @@
+import { observable } from '@legendapp/state';
+
+interface SpecObserver<T = unknown, E = unknown> {
+    next: (value: T) => void;
+    error: (error: E) => void;
+    complete: () => void;
+}
+
+interface SpecSubscription {
+    unsubscribe(): void;
+    closed?: boolean;
+}
+
+interface SpecObservable<T> {
+    subscribe: (observer: SpecObserver<T, any>) => SpecSubscription;
+}
+
+export function fromSpecObservable<T = unknown>(specObservable: SpecObservable<T>, initialValue?: T) {
+    const obs = observable<T>(initialValue);
+    specObservable.subscribe({
+        next: (value) => {
+            obs.set((_: T) => value);
+        },
+        error: (error) => {},
+        complete: () => {},
+    });
+
+    return obs;
+}
