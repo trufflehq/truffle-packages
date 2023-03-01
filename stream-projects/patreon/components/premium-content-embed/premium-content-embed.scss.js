2ff39d4d4b222960c4de36714ae7e1e49e929b2b
Austin Hallock <austinhallock@gmail.com>
2023-03-01 11:28:01 -0800

    link to community discord for patrons

--- stream-projects/patreon/components/premium-content-embed/premium-content-embed.scss.js
+++ stream-projects/patreon/components/premium-content-embed/premium-content-embed.scss.js
@@ -5,121 +5,160 @@ export default scss`
   width: 100%;
   height: 100%;
   overflow: hidden;
-  box-sizing: border-box;
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
   font-family: Roboto;
-  position: relative;
-  display: block;
-  text-decoration: none;
 
-  &.is-collapsed {
+  > .patreon-content {
+    width: 100%;
+    box-sizing: border-box;
+    border: 1px solid rgba(255, 255, 255, 0.2);
+    border-radius: 10px;
+    position: relative;
+    display: block;
+    text-decoration: none;
+
+    &.is-collapsed {
+      > .top {
+        padding: 8px;
+        height: 30px;
+
+        > .info {
+          > .description {
+            display: none;
+          }
+        }
+      }
+    }
+
     > .top {
-      padding: 8px;
-      height: 30px;
+      text-decoration: none;
+      width: 100%;
+      padding: 12px 16px;
+      box-sizing: border-box;
+      background: #292829;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+
+      > .icon {
+        margin-right: 16px;
+      }
 
       > .info {
+        > .title {
+          font-weight: 600;
+          font-size: 16px;
+          line-height: 19px;
+        }
+
         > .description {
-          display: none;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 16px;
+          color: rgba(255, 255, 255, 0.7);
+          margin-top: 2px;
         }
       }
-    }
-  }
 
-  > .top {
-    text-decoration: none;
-    width: 100%;
-    padding: 12px 16px;
-    box-sizing: border-box;
-    background: #292829;
-    color: #fff;
-    display: flex;
-    align-items: center;
-
-    > .icon {
-      margin-right: 16px;
-    }
-
-    > .info {
-      > .title {
-        font-weight: 600;
-        font-size: 16px;
-        line-height: 19px;
-      }
-
-      > .description {
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 16px;
-        color: rgba(255, 255, 255, 0.7);
-        margin-top: 2px;
+      > .close {
+        margin-left: auto;
       }
     }
 
-    > .close {
-      margin-left: auto;
-    }
-  }
+    > .content {
+      padding: 16px;
+      background: #111;
+      color: #fff;
+      font-weight: 600;
+      font-size: 16px;
+      border-bottom-left-radius: 10px;
+      border-bottom-right-radius: 10px;
 
-  > .content {
-    padding: 16px;
-    background: #111;
-    color: #fff;
-    font-weight: 600;
-    font-size: 16px;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-
-    > .preview {
-      height: 208px;
-      margin-bottom: 16px;
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      gap: 12px;
-      // give blur hard edges
-      // https://stackoverflow.com/a/23370117
-      overflow: hidden;
-      -webkit-transform: translate3d(0, 0, 0);
-
-      > .background {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background-size: cover;
-        filter: blur(4px);
-        z-index: -1;
-      }
-
-      > .button {
+      > .preview {
+        height: 208px;
+        margin-bottom: 16px;
+        position: relative;
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
-        gap: 10px;
-        text-decoration: none;
-        text-align: center;
-        padding: 6px 16px;
-        background: #FF424D;
-        border-radius: 30px;
-        color: #000;
-        font-weight: 600;
-        font-size: 14px;
-        cursor: pointer;
+        gap: 12px;
+        // give blur hard edges
+        // https://stackoverflow.com/a/23370117
+        overflow: hidden;
+        -webkit-transform: translate3d(0, 0, 0);
 
-        &:hover {
-          background: #db3842;
+        > .background {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          background-size: cover;
+          filter: blur(4px);
+          z-index: -1;
+        }
+
+        > .button {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 10px;
+          text-decoration: none;
+          text-align: center;
+          padding: 6px 16px;
+          background: #FF424D;
+          border-radius: 30px;
+          color: #000;
+          font-weight: 600;
+          font-size: 14px;
+          cursor: pointer;
+
+          &:hover {
+            background: #db3842;
+          }
+        }
+      }
+
+      > .info {
+        > .title {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
         }
       }
     }
+  }
 
-    > .info {
-      > .title {
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
+  .discord-promo {
+    margin-top: 20px;
+    color: #fff;
+    display: flex;
+    font-size: 13px;
+    font-weight: 400;
+    padding: 0 8px;
+  
+    > .logos {
+      display: flex;
+      gap: 8px;
+      margin-right: 12px;
+  
+      > .logo {
+        width: 16px;
+        height: 16px;
       }
+  
+      > .divider {
+        width: 1px;
+        height: 100%;
+        background: rgba(255, 255, 255, 0.3);
+      }
+    }
+  
+    > a {
+      color: #F357A1;
+      margin-left: 4px;
     }
   }
 }`;
--- stream-projects/patreon/truffle.config.mjs
+++ stream-projects/patreon/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/patreon",
-  version: "0.5.49",
+  version: "0.5.52",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Patreon integration for Truffle",
   requestedPermissions: [],
