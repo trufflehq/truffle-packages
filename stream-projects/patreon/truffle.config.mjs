bacb4848b17f51545e08fbc3e0c09245a01f03ea
Austin Hallock <austinhallock@gmail.com>
2023-03-02 14:32:05 -0800

    bumps

--- stream-projects/patreon/truffle.config.mjs
+++ stream-projects/patreon/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/patreon",
-  version: "0.5.52",
+  version: "0.5.57",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Patreon integration for Truffle",
   requestedPermissions: [],
