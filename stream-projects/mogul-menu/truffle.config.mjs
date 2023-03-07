d07cb92f8ca743d377ff49c661f1c8325edcc5ef
Matthew Kline <matthewwkline@gmail.com>
2023-03-06 23:34:06 -0700

    Update version number

--- stream-projects/mogul-menu/truffle.config.mjs
+++ stream-projects/mogul-menu/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "3.0.155",
+  version: "3.0.156",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Menu for channel points, predictions, and activities",
   requestedPermissions: [],
