291a659f2b2f0edba5602095e614f09f0e1a92c7
Austin Fay <afay@truffle.vip>
2023-03-13 09:09:33 -0600

    feat: switch sdk to transframe

--- npm/sdk/src/constants.ts
+++ npm/sdk/src/constants.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_MYCELIUM_API_URL = 'https://mycelium.truffle.vip/graphql';
+export const DEFAULT_MYCELIUM_API_URL = 'https://mycelium.staging.bio/graphql';
--- npm/sdk/src/user/access-token.ts
+++ npm/sdk/src/user/access-token.ts
@@ -1,6 +1,9 @@
 import { embedConsumer } from '../transframe/embed-consumer';
 
 export async function getAccessToken(apiUrl?: string): Promise<string> {
+  // FIXME: This is a hack to wait for the embed transframe provider to initialize
+  await new Promise((resolve) => setTimeout(resolve, 10));
+
   const token = (await embedConsumer.call('userGetAccessToken')) as string;
   return token;
 }
