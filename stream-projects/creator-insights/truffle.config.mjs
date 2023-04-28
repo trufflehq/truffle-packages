22d85bf97e3d1f8810f31cfca35a1bd8bdd1febf
Austin Hallock <austinhallock@gmail.com>
2023-04-27 21:39:48 -0700

    bump creator-insights

--- stream-projects/creator-insights/truffle.config.mjs
+++ stream-projects/creator-insights/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/creator-insights",
-  version: "0.5.34",
+  version: "0.5.35",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Truffle Creator Insights",
   requestedPermissions: [],
