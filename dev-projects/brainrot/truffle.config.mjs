6ca5fa339a081187bbca8260b9f24c8a07dad53b
Austin Fay <austin@austinfay.com>
2023-05-03 17:58:11 -0600

    chore: add embed back to brainrotter

--- dev-projects/brainrot/truffle.config.mjs
+++ dev-projects/brainrot/truffle.config.mjs
@@ -3,6 +3,15 @@ export default {
   version: "0.5.30",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "The Ultimate Brainrotter",
+  embeds: [
+    {
+      slug: "brainrot-twitch-embed",
+      url: "https://brainrot-embed.netlify.app/",
+      contentPageType: "twitch",
+      parentQuerySelector: "body",
+      defaultStyles: {},
+    },
+  ],
   requestedPermissions: [],
   installActionRel: {},
 };
