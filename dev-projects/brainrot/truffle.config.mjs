78207d3cbae2c729b2c9e40e84e78c7ed176300a
Austin Fay <afay@truffle.vip>
2023-05-03 17:52:40 -0600

    add: brainrot

---
+++ dev-projects/brainrot/truffle.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  name: "@cucumber-owl/brainrot",
+  version: "0.5.30",
+  apiUrl: "https://mycelium.truffle.vip/graphql",
+  description: "The Ultimate Brainrotter",
+  requestedPermissions: [],
+  installActionRel: {},
+};
