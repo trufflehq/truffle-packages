7c5a66f1a94e178d49fc0d377dd93b8a0d0c5825
Austin Fay <austin@austinfay.com>
2023-05-03 18:30:57 -0600

    add: revojam

---
+++ dev-projects/revojam/truffle.config.mjs
@@ -0,0 +1,45 @@
+export default {
+  name: "@alexswear/revojam",
+  version: "0.5.30",
+  apiUrl: "https://mycelium.truffle.vip/graphql",
+  description:
+    "the ultimate music companion for livestreamers. collaborate with viewers on live playlists.",
+  requestedPermissions: [],
+  installActionRel: {},
+  embeds: [
+    {
+      slug: "revojam-youtube-embed",
+      contentPageType: "youtube",
+      defaultStyles: {
+        "background-color": "#666",
+        "border": "2px solid black",
+        "border-radius": "3px",
+        "box-shadow": "2px 3px black",
+        "height": "85px",
+        "margin-top": "3px",
+        "transition": "height 300ms ease-in-out",
+        "width": "100%",
+      },
+      insertionMethod: "prepend",
+      parentQuerySelector: "#above-the-fold",
+      url: "https://app.revojam.com/truffleConnect",
+    },
+    {
+      slug: "revojam-twitch-embed",
+      contentPageType: "twitch",
+      defaultStyles: {
+        "background-color": "#666",
+        "border": "2px solid black",
+        "border-radius": "3px",
+        "box-shadow": "2px 3px black",
+        "height": "85px",
+        "margin-top": "3px",
+        "transition": "height 300ms ease-in-out",
+        "width": "100%",
+      },
+      insertionMethod: "prepend",
+      parentQuerySelector: ".channel-info-content",
+      url: "https://app.revojam.com/truffleConnect",
+    },
+  ],
+};
