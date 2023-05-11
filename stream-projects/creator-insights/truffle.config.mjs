e44a58ab49f9df916939805fdd8473e07525e635
Austin Hallock <austinhallock@gmail.com>
2023-05-10 18:50:22 -0700

    handle creator video -> creator video better (embed isn't reloaded)

--- stream-projects/creator-insights/truffle.config.mjs
+++ stream-projects/creator-insights/truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/creator-insights",
-  version: "0.5.35",
+  version: "0.5.37",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Truffle Creator Insights",
   requestedPermissions: [],
