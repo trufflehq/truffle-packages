a41767d0a6d07a640b4b88f199c42a69c1a8d4a4
Austin Fay <afay@truffle.vip>
2023-05-31 22:08:13 -0600

    refactor: clean up example

--- npm/transframe/examples/multi-child/src/api.ts
+++ npm/transframe/examples/multi-child/src/api.ts
@@ -7,26 +7,26 @@ export const frameCallbackMap: Map<string, FrameCallback> = new Map();
 export const globalCallbacks: Function[] = [];
 
 export const api = createIframeApi({
-  sayHello ({ fromId }) {
+  sayHello({ fromId }) {
     println(`Saying hello to ${fromId}!`);
     return `Hello!`;
   },
-  getId ({ fromId }) {
+  getId({ fromId }) {
     return fromId;
   },
-  registerFrameCallback ({ fromId }, callback: FrameCallback) {
+  registerFrameCallback({ fromId }, callback: FrameCallback) {
     // ignore if we don't have an id
     if (!fromId) return;
 
     println(`Registering frame callback for ${fromId}`);
-    console.log('Registering callback', fromId, callback)
+    console.log("Registering callback", fromId, callback);
     frameCallbackMap.set(fromId, callback);
   },
-  registerGlobalCallback ({ fromId }, callback) {
+  registerGlobalCallback({ fromId }, callback: FrameCallback) {
     println(`Registering global callback for ${fromId}`);
     globalCallbacks.push(callback);
   },
   throwError() {
     throw new Error("This is an error!");
-  }
+  },
 });
--- npm/transframe/examples/multi-child/src/child.ts
+++ npm/transframe/examples/multi-child/src/child.ts
@@ -1,41 +1,43 @@
-import { TransframeConsumer } from "@trufflehq/transframe";
-import { IframeConsumerInterface } from "@trufflehq/transframe/iframe";
-import { api as backendApi } from "./api";
+import { createIframeConsumer } from "@trufflehq/transframe/iframe";
+import { api as providerApi } from "./api";
 import { println } from "./console";
 
-const iframeConsumer = new TransframeConsumer<typeof backendApi>(
-  new IframeConsumerInterface(),
-  {
-    namespace: 'truffle-site'
-  }
-);
+const iframeConsumer = createIframeConsumer<typeof providerApi>({
+  namespace: "truffle-site",
+});
 
 const api = iframeConsumer.api;
 
 api.registerFrameCallback((message) => {
-  println('A message just for meee!');
-  println('It says: ' + message);
-})
+  println("A message just for meee!");
+  println("It says: " + message);
+});
 
 api.registerGlobalCallback(() => {
-  println('A message for everyone!')
-})
+  println("A message for everyone!");
+});
 
-const sayHelloButton = document.querySelector('#say-hello-button') as HTMLButtonElement;
-const getIdButton = document.querySelector('#get-id-button') as HTMLButtonElement;
-const throwErrorButton = document.querySelector('#throw-error-button') as HTMLButtonElement;
+const sayHelloButton = document.querySelector(
+  "#say-hello-button"
+) as HTMLButtonElement;
+const getIdButton = document.querySelector(
+  "#get-id-button"
+) as HTMLButtonElement;
+const throwErrorButton = document.querySelector(
+  "#throw-error-button"
+) as HTMLButtonElement;
 
-sayHelloButton.addEventListener('click', async () => {
+sayHelloButton.addEventListener("click", async () => {
   const response = await api.sayHello();
   println(`From parent: ${response}`);
 });
 
-getIdButton.addEventListener('click', async () => {
+getIdButton.addEventListener("click", async () => {
   const response = await api.getId();
   println(`From parent: ${response}`);
 });
 
-throwErrorButton.addEventListener('click', async () => {
+throwErrorButton.addEventListener("click", async () => {
   try {
     await api.throwError();
   } catch (error) {
