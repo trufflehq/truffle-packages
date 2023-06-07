c6791b2ca942e72a5ef40287461b566c855cd633
Austin Fay <afay@truffle.vip>
2023-06-07 16:11:34 -0600

    fix: global theme vars and some cleanup

--- stream-projects/mogul-menu-v2/src/shared/util/truffle/truffle-app.ts
+++ stream-projects/mogul-menu-v2/src/shared/util/truffle/truffle-app.ts
@@ -2,7 +2,6 @@ import { TruffleApp } from "https://npm.tfl.dev/@trufflehq/sdk@0.2.3";
 import { getAccessToken } from "./access-token.ts";
 import { getOrgId } from "./org-id.ts";
 
-console.log("TruffleApp", TruffleApp);
 export const truffleApp = new TruffleApp({
   orgId: getOrgId(),
   url: "https://mycelium.truffle.vip/graphql",
