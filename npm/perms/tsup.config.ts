0e89783b8567e3049461155021c09159afa22cde
Austin Fay <afay@truffle.vip>
2023-08-04 09:22:52 -0600

    refactor: made perms an actual package

---
+++ npm/perms/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  globalName: "TrufflePerms",
+  clean: true,
+  dts: true,
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  minify: false,
+  skipNodeModulesBundle: true,
+  sourcemap: true,
+  target: "es2021",
+  tsconfig: "./tsconfig.json",
+  keepNames: false, // keepNames: true can do some weird stuff (search keepNames in our discord)
+  treeshake: true,
+});
